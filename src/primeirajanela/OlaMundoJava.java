f06ebbb7a8b84b78eb77b75d463f1071f047fcc2
Michael Robert Coitim <michaelcoitim1@gmail.com>
2021-03-25 23:25:23 -0300

    ajuste na mensagem

--- src/primeirajanela/OlaMundoJava.java
+++ src/primeirajanela/OlaMundoJava.java
@@ -75,7 +75,7 @@ public class OlaMundoJava extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnClickActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnClickActionPerformed
-         lblMensagem.setText("Ola, Mundo!");
+         lblMensagem.setText("Olá, Mundo!");
     }//GEN-LAST:event_btnClickActionPerformed
 
     /**
